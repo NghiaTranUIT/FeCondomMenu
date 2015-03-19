800a8c99d59ecfaf6da146c5b9eae99e47feb31b
NghiaTran <changeyourcolor@gmail.com>
2015-03-19 15:06:56 +0700

    Initial Commit

---
+++ FeCondomMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FeCondomMenu
+//
+//  Created by Nghia Tran on 3/19/15.
+//  Copyright (c) 2015 Fe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
